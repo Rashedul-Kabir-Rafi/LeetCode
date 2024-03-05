4dce0e5f2b4f168e6574031d6c03febdde1e0dbe
Rasshedul Kabir Rafi <125783116+Rashedul-Kabir-Rafi@users.noreply.github.com>
2024-03-05 20:17:37 +0600

    Time: 37 ms (41.81%), Space: 16.6 MB (44.92%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -1,3 +1,7 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
+        new = ''
+        for i in s:
+            i = chr(ord(i) + 32) if i.isupper() else i
+            new+=i
+        return new
