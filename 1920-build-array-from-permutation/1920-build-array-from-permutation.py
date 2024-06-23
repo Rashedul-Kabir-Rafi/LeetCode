405a3151efcbf7fbdc6f5d8e7485eab02c14fd2f
Rasshedul Kabir Rafi <125783116+Rashedul-Kabir-Rafi@users.noreply.github.com>
2024-06-23 23:46:50 +0600

    Time: 95 ms (62.62%), Space: 16.7 MB (96.07%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,3 +1,3 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        return list(map(lambda i: nums[nums[i]], range(len(nums))))
+        return [nums[nums[i]] for i in range(len(nums))]
